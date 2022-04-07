7fee08f89968c3764168ed5c8d33e19b71c3eb21
Akshit Bansal <bansalakshit56@gmail.com>
2022-04-07 13:24:57 +0530

    tried 3sum

--- src/main/kotlin/3Sum with Multiplicity.kt
+++ src/main/kotlin/3Sum with Multiplicity.kt
@@ -1,5 +1,28 @@
 class Solution {
     fun threeSumMulti(arr: IntArray, target: Int): Int {
+        val MODULO = 1000000007
 
+        var ans = 0
+        val end = arr.lastIndex
+        for (i in 0..end - 2) {
+            ans += twoSum(arr, i + 1, end, target - arr[i]) % MODULO
+        }
+
+        return ans
+    }
+
+    fun twoSum(arr: IntArray, start: Int, end: Int, target: Int): Int {
+        var couples = 0
+        val hashMap = mutableMapOf<Int, Int>()
+
+        for (i in start..end) {
+            couples += hashMap.getOrDefault(target - arr[i], 0)
+
+            if (hashMap[arr[i]] == null) {
+                hashMap[arr[i]] = 1
+            } else hashMap[arr[i]] = hashMap[arr[i]]!!.plus(1)
+        }
+
+        return couples
     }
 }
