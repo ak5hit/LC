db9cd8d5ea681b3bb4efb6ab4f2df2360ed5233e
akshit bansal <akshit.bansal@udaan.com>
2022-04-08 01:09:24 +0530

    7 apr done kotlin

--- src/main/kotlin/LastStoneWeight.kt
+++ src/main/kotlin/LastStoneWeight.kt
@@ -1,2 +1,19 @@
-class LastStoneWeight {
+import java.util.*
+
+class Solution {
+    fun lastStoneWeight(stones: IntArray): Int {
+        val heap = PriorityQueue<Int>(stones.size) { a, b -> b - a }
+
+//        stones.forEach { heap.add(it) }
+        for (stone in stones) {
+            heap.add(stone)
+        }
+        while (heap.size > 1) {
+            val a = heap.poll()
+            val b = heap.poll()
+            if (a != b) heap.add(a - b)
+        }
+
+        return if (heap.isEmpty()) 0 else heap.peek()
+    }
 }
