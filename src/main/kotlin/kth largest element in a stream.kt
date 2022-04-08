7614a2bb9cdb2a5e454c3f25b52a6d064251cd29
akshit bansal <akshit.bansal@udaan.com>
2022-04-09 01:16:53 +0530

    ..

--- src/main/kotlin/kth largest element in a stream.kt
+++ src/main/kotlin/kth largest element in a stream.kt
@@ -1,2 +1,13 @@
-class `kth largest element in a stream` {
+class KthLargest(k: Int, nums: IntArray) {
+
+    fun add(`val`: Int): Int {
+        
+    }
+
 }
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * var obj = KthLargest(k, nums)
+ * var param_1 = obj.add(`val`)
+ */
